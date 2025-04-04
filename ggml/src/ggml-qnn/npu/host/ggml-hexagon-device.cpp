c80c54748951bacca91d6a55d3f48eb986bec90f
hongruichen <chraac@gmail.com>
2025-04-04 15:38:17 +0800

    wip

--- ggml/src/ggml-qnn/npu/host/ggml-hexagon-device.cpp
+++ ggml/src/ggml-qnn/npu/host/ggml-hexagon-device.cpp
@@ -1,6 +1,8 @@
 
 #include "common.hpp"
 
+namespace {}  // namespace
+
 backend_device_proxy_ptr create_hexagon_backend_context(backend_index_type device) {
     if (device < QNN_BACKEND_COUNT || device >= TOTAL_BACKEND_COUNT) {
         return backend_device_proxy_ptr();
