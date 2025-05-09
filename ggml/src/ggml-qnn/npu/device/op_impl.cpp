26e6e0123f0cffb8af20fbb90318f21731b306db
hongruichen <chraac@gmail.com>
2025-05-09 13:27:29 +0800

    wip

--- ggml/src/ggml-qnn/npu/device/op_impl.cpp
+++ ggml/src/ggml-qnn/npu/device/op_impl.cpp
@@ -294,7 +294,7 @@ compute_func_type get_compute_func(tensor * dst) {
 
 bool support_op(const npu_device_tensor_spec & src0, const npu_device_tensor_spec & src1,
                 const npu_device_tensor_spec & dst, npu_device_tensor_op op) {
-    if (!get_compute_func_impl(op, dst.type)) {
+    if (get_compute_func_impl(op, dst.type) == nullptr) {
         DEVICE_LOG_ERROR("[%s]unsupported, get_compute_func failed\n", op_get_name(op));
         return false;
     }
