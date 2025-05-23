edfb96393c0d89d7a652c3cee0e7940a2f220f99
hongruichen <chraac@gmail.com>
2025-05-24 01:24:27 +0800

    rename

--- ggml/src/ggml-qnn/npu/device/op_mul_mat.cpp
+++ ggml/src/ggml-qnn/npu/device/op_mul_mat.cpp
@@ -13,18 +13,18 @@ inline float vec_dot_product_f32_f32(const float * src0, const float * src1, siz
 
     const size_t vec_count = count / kElementsPerVector;
 
-    HVX_Vector * iptr0     = ((HVX_Vector *) src0);
-    HVX_Vector * iptr0_end = ((HVX_Vector *) src0) + vec_count;
-    HVX_Vector * iptr1     = ((HVX_Vector *) src1);
-    HVX_Vector   prev0     = *iptr0++;
-    HVX_Vector   prev1     = *iptr1++;
-    HVX_Vector   sum       = Q6_V_vzero();
+    HVX_Vector * src0_vec_ptr     = ((HVX_Vector *) src0);
+    HVX_Vector * src0_vec_ptr_end = ((HVX_Vector *) src0) + vec_count;
+    HVX_Vector * src1_vec_ptr     = ((HVX_Vector *) src1);
+    HVX_Vector   prev0            = *src0_vec_ptr++;
+    HVX_Vector   prev1            = *src1_vec_ptr++;
+    HVX_Vector   sum              = Q6_V_vzero();
 
-    while (iptr0_end - iptr0 > 1) {
-        HVX_Vector curr0_lo = iptr0[0];
-        HVX_Vector curr0_hi = iptr0[1];
-        HVX_Vector curr1_lo = iptr1[0];
-        HVX_Vector curr1_hi = iptr1[1];
+    while (src0_vec_ptr_end - src0_vec_ptr > 1) {
+        HVX_Vector curr0_lo = src0_vec_ptr[0];
+        HVX_Vector curr0_hi = src0_vec_ptr[1];
+        HVX_Vector curr1_lo = src1_vec_ptr[0];
+        HVX_Vector curr1_hi = src1_vec_ptr[1];
 
         HVX_Vector s0 = Q6_V_valign_VVR(curr0_lo, prev0, (size_t) src0);
         HVX_Vector s1 = Q6_V_valign_VVR(curr1_lo, prev1, (size_t) src1);
@@ -35,13 +35,13 @@ inline float vec_dot_product_f32_f32(const float * src0, const float * src1, siz
 
         prev0 = curr0_hi;
         prev1 = curr1_hi;
-        iptr0 += 2;
-        iptr1 += 2;
+        src0_vec_ptr += 2;
+        src1_vec_ptr += 2;
     }
 
-    if (iptr0_end - iptr0 > 0) {
-        HVX_Vector curr0 = *iptr0++;
-        HVX_Vector curr1 = *iptr1++;
+    if (src0_vec_ptr_end - src0_vec_ptr > 0) {
+        HVX_Vector curr0 = *src0_vec_ptr++;
+        HVX_Vector curr1 = *src1_vec_ptr++;
         HVX_Vector s0    = Q6_V_valign_VVR(curr0, prev0, (size_t) src0);
         HVX_Vector s1    = Q6_V_valign_VVR(curr1, prev1, (size_t) src1);
         sum              = Q6_Vqf32_vadd_Vqf32Vqf32(Q6_Vqf32_vmpy_VsfVsf(s0, s1), sum);
@@ -49,17 +49,17 @@ inline float vec_dot_product_f32_f32(const float * src0, const float * src1, siz
         prev1            = curr1;
     }
 
-    if ((iptr0_end - ((HVX_Vector *) src0)) > 0) {
+    if ((src0_vec_ptr_end - ((HVX_Vector *) src0)) > 0) {
         // handle the last vector
         // see also:
         //   https://github.com/UbiquitousLearning/mllm/blob/babf4410352ce8730824c87699c025a0d4ce3a6f/src/backends/qnn/LLaMAOpPackageHtp/LLaMAPackage/src/ops/LLaMAMul.cpp#L147
         //   or qualcomm sdk libs\qhl_hvx\src\qhblas_hvx\qhblas_hvx_aw_vector_add_ah.c
-        bool       iptr0_aligned = hexagon::is_addr_aligned(iptr0);
-        HVX_Vector curr0         = iptr0_aligned ? prev0 : *iptr0;
-        iptr0                    = iptr0_aligned ? iptr0 : iptr0 + 1;
-        bool       iptr1_aligned = hexagon::is_addr_aligned(iptr1);
-        HVX_Vector curr1         = iptr1_aligned ? prev1 : *iptr1;
-        iptr1                    = iptr1_aligned ? iptr1 : iptr1 + 1;
+        bool       iptr0_aligned = hexagon::is_addr_aligned(src0_vec_ptr);
+        HVX_Vector curr0         = iptr0_aligned ? prev0 : *src0_vec_ptr;
+        src0_vec_ptr             = iptr0_aligned ? src0_vec_ptr : src0_vec_ptr + 1;
+        bool       iptr1_aligned = hexagon::is_addr_aligned(src1_vec_ptr);
+        HVX_Vector curr1         = iptr1_aligned ? prev1 : *src1_vec_ptr;
+        src1_vec_ptr             = iptr1_aligned ? src1_vec_ptr : src1_vec_ptr + 1;
         HVX_Vector s0            = Q6_V_valign_VVR(curr0, prev0, (size_t) src0);
         HVX_Vector s1            = Q6_V_valign_VVR(curr1, prev1, (size_t) src1);
         sum                      = Q6_Vqf32_vadd_Vqf32Vqf32(Q6_Vqf32_vmpy_VsfVsf(s0, s1), sum);
@@ -71,13 +71,15 @@ inline float vec_dot_product_f32_f32(const float * src0, const float * src1, siz
     const size_t leftover_bytes = leftover * sizeof(float);
     if (leftover > 0) {
         // handle the leftover elements
-        HVX_Vector curr0 =
-            (leftover_bytes + hexagon::unaligned_bytes(iptr0) > hexagon::kBytesPerVector) ? *iptr0 : prev0;
-        curr0 = Q6_V_valign_VVR(curr0, prev0, (size_t) src0);
+        HVX_Vector curr0 = (leftover_bytes + hexagon::unaligned_bytes(src0_vec_ptr) > hexagon::kBytesPerVector) ?
+                               *src0_vec_ptr :
+                               prev0;
+        curr0            = Q6_V_valign_VVR(curr0, prev0, (size_t) src0);
 
-        HVX_Vector curr1 =
-            (leftover_bytes + hexagon::unaligned_bytes(iptr1) > hexagon::kBytesPerVector) ? *iptr1 : prev1;
-        curr1 = Q6_V_valign_VVR(curr1, prev1, (size_t) src1);
+        HVX_Vector curr1 = (leftover_bytes + hexagon::unaligned_bytes(src1_vec_ptr) > hexagon::kBytesPerVector) ?
+                               *src1_vec_ptr :
+                               prev1;
+        curr1            = Q6_V_valign_VVR(curr1, prev1, (size_t) src1);
 
         sum = Q6_Vqf32_vadd_Vqf32Vqf32(
             Q6_V_valign_VVR(Q6_Vqf32_vmpy_VsfVsf(curr0, curr1), Q6_V_vzero(), leftover_bytes), sum);
@@ -91,17 +93,17 @@ inline float vec_dot_product_f16_f16(const npu_device_fp16_t * src0, const npu_d
     constexpr const size_t kElementsPerVector = hexagon::kBytesPerVector / sizeof(npu_device_fp16_t);
     constexpr const size_t kFloatsPerVector   = hexagon::kBytesPerVector / sizeof(float);
 
-    HVX_Vector * iptr0     = ((HVX_Vector *) src0);
-    HVX_Vector * iptr0_end = ((HVX_Vector *) src0) + (count / kElementsPerVector);
-    HVX_Vector * iptr1     = ((HVX_Vector *) src1);
-    HVX_Vector   prev0     = *iptr0++;
-    HVX_Vector   prev1     = *iptr1++;
-    HVX_Vector   sum_hi    = Q6_V_vzero();
-    HVX_Vector   sum_lo    = Q6_V_vzero();
+    HVX_Vector * src0_vec_ptr     = ((HVX_Vector *) src0);
+    HVX_Vector * src0_vec_ptr_end = ((HVX_Vector *) src0) + (count / kElementsPerVector);
+    HVX_Vector * src1_vec_ptr     = ((HVX_Vector *) src1);
+    HVX_Vector   prev0            = *src0_vec_ptr++;
+    HVX_Vector   prev1            = *src1_vec_ptr++;
+    HVX_Vector   sum_hi           = Q6_V_vzero();
+    HVX_Vector   sum_lo           = Q6_V_vzero();
 
-    while (iptr0 < iptr0_end) {
-        HVX_Vector     curr0  = *iptr0++;
-        HVX_Vector     curr1  = *iptr1++;
+    while (src0_vec_ptr < src0_vec_ptr_end) {
+        HVX_Vector     curr0  = *src0_vec_ptr++;
+        HVX_Vector     curr1  = *src1_vec_ptr++;
         HVX_Vector     s0     = Q6_V_valign_VVR(curr0, prev0, (size_t) src0);
         HVX_Vector     s1     = Q6_V_valign_VVR(curr1, prev1, (size_t) src1);
         HVX_VectorPair result = Q6_Wqf32_vmpy_VhfVhf(s0, s1);
@@ -111,17 +113,17 @@ inline float vec_dot_product_f16_f16(const npu_device_fp16_t * src0, const npu_d
         prev1                 = curr1;
     }
 
-    if ((iptr0_end - ((HVX_Vector *) src0)) > 0) {
+    if ((src0_vec_ptr_end - ((HVX_Vector *) src0)) > 0) {
         // handle the last vector
         // see also:
         //   https://github.com/UbiquitousLearning/mllm/blob/babf4410352ce8730824c87699c025a0d4ce3a6f/src/backends/qnn/LLaMAOpPackageHtp/LLaMAPackage/src/ops/LLaMAMul.cpp#L147
         //   or qualcomm sdk libs\qhl_hvx\src\qhblas_hvx\qhblas_hvx_aw_vector_add_ah.c
-        bool       iptr0_aligned = hexagon::is_addr_aligned(iptr0);
-        HVX_Vector curr0         = iptr0_aligned ? prev0 : *iptr0;
-        iptr0                    = iptr0_aligned ? iptr0 : iptr0 + 1;
-        bool       iptr1_aligned = hexagon::is_addr_aligned(iptr1);
-        HVX_Vector curr1         = iptr1_aligned ? prev1 : *iptr1;
-        iptr1                    = iptr1_aligned ? iptr1 : iptr1 + 1;
+        bool       iptr0_aligned = hexagon::is_addr_aligned(src0_vec_ptr);
+        HVX_Vector curr0         = iptr0_aligned ? prev0 : *src0_vec_ptr;
+        src0_vec_ptr             = iptr0_aligned ? src0_vec_ptr : src0_vec_ptr + 1;
+        bool       iptr1_aligned = hexagon::is_addr_aligned(src1_vec_ptr);
+        HVX_Vector curr1         = iptr1_aligned ? prev1 : *src1_vec_ptr;
+        src1_vec_ptr             = iptr1_aligned ? src1_vec_ptr : src1_vec_ptr + 1;
         HVX_Vector     s0        = Q6_V_valign_VVR(curr0, prev0, (size_t) src0);
         HVX_Vector     s1        = Q6_V_valign_VVR(curr1, prev1, (size_t) src1);
         HVX_VectorPair result    = Q6_Wqf32_vmpy_VhfVhf(s0, s1);
@@ -135,13 +137,15 @@ inline float vec_dot_product_f16_f16(const npu_device_fp16_t * src0, const npu_d
     const size_t leftover_bytes = leftover * sizeof(npu_device_fp16_t);
     if (leftover > 0) {
         // handle the leftover elements
-        HVX_Vector curr0 =
-            (leftover_bytes + hexagon::unaligned_bytes(iptr0) > hexagon::kBytesPerVector) ? *iptr0 : prev0;
-        curr0 = Q6_V_valign_VVR(curr0, prev0, (size_t) src0);
+        HVX_Vector curr0 = (leftover_bytes + hexagon::unaligned_bytes(src0_vec_ptr) > hexagon::kBytesPerVector) ?
+                               *src0_vec_ptr :
+                               prev0;
+        curr0            = Q6_V_valign_VVR(curr0, prev0, (size_t) src0);
 
-        HVX_Vector curr1 =
-            (leftover_bytes + hexagon::unaligned_bytes(iptr1) > hexagon::kBytesPerVector) ? *iptr1 : prev1;
-        curr1 = Q6_V_valign_VVR(curr1, prev1, (size_t) src1);
+        HVX_Vector curr1 = (leftover_bytes + hexagon::unaligned_bytes(src1_vec_ptr) > hexagon::kBytesPerVector) ?
+                               *src1_vec_ptr :
+                               prev1;
+        curr1            = Q6_V_valign_VVR(curr1, prev1, (size_t) src1);
 
         HVX_VectorPair result = Q6_Wqf32_vmpy_VhfVhf(curr0, curr1);
 
