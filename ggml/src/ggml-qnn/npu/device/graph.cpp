e2ba224551f1ccb3ce42934725f6900e6a55c49e
hongruichen <chraac@gmail.com>
2025-06-10 23:30:21 +0800

    invalidate last tensor in compute_impl

--- ggml/src/ggml-qnn/npu/device/graph.cpp
+++ ggml/src/ggml-qnn/npu/device/graph.cpp
@@ -90,7 +90,10 @@ void graph::compute_impl(default_thread_pool * pool, default_thread_pool::thread
         if (pool && should_sync && i < _tensor_count - 1) {
             pool->sync_thread();
         }
-        dst->invalidate();
+    }
+
+    if (_tensor_count > 0) {
+        _tensors[_tensor_count - 1]->invalidate();
     }
 }
 
