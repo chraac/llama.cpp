8da9e8e7be587139f81ce87f61f8c9b802bbfe24
hongruichen <chraac@gmail.com>
2025-06-10 23:18:17 +0800

    Revert "restrict tensor invalidation to the first thread in compute_impl"
    This reverts commit 0a8ff2b1bcf366097c16d7437c091382eacbef8b.

--- ggml/src/ggml-qnn/npu/device/graph.cpp
+++ ggml/src/ggml-qnn/npu/device/graph.cpp
@@ -90,10 +90,7 @@ void graph::compute_impl(default_thread_pool * pool, default_thread_pool::thread
         if (pool && should_sync && i < _tensor_count - 1) {
             pool->sync_thread();
         }
-
-        if (params.get_thread_index() == 0) {
-            dst->invalidate();
-        }
+        dst->invalidate();
     }
 }
 
