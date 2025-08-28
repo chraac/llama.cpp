5318d9bb09c9595249f217802b1b7597132ef6fe
chraac <chraac@gmail.com>
2025-08-28 21:31:19 +0800

    Enhance DMA transfer functionality by adding 2D submission support and improving descriptor initialization

--- ggml/src/ggml-qnn/npu/device/dma_transfer.cpp
+++ ggml/src/ggml-qnn/npu/device/dma_transfer.cpp
@@ -13,19 +13,30 @@ dma_transfer::dma_transfer() {
     dma_desc_set_dstate(_dma_1d_desc0, DESC_DSTATE_INCOMPLETE);
     dma_desc_set_desctype(_dma_1d_desc0, DMA_DESC_TYPE_1D);
 
-    dma_desc_set_order(_dma_1d_desc0, 1);
-    dma_desc_set_bypasssrc(_dma_1d_desc0, 1);  // for dram
-    dma_desc_set_bypassdst(_dma_1d_desc0, 0);  // for vtcm
+    dma_desc_set_order(_dma_1d_desc0, DESC_ORDER_ORDER);
+    dma_desc_set_bypasssrc(_dma_1d_desc0, DESC_BYPASS_ON);   // for dram
+    dma_desc_set_bypassdst(_dma_1d_desc0, DESC_BYPASS_OFF);  // for vtcm
     dma_desc_set_length(_dma_1d_desc0, 0);
 
     dma_desc_set_next(_dma_1d_desc1, 0);
     dma_desc_set_dstate(_dma_1d_desc1, DESC_DSTATE_INCOMPLETE);
     dma_desc_set_desctype(_dma_1d_desc1, DMA_DESC_TYPE_1D);
 
-    dma_desc_set_order(_dma_1d_desc1, 1);
-    dma_desc_set_bypasssrc(_dma_1d_desc1, 1);  // for dram
-    dma_desc_set_bypassdst(_dma_1d_desc1, 0);  // for vtcm
+    dma_desc_set_order(_dma_1d_desc1, DESC_ORDER_ORDER);
+    dma_desc_set_bypasssrc(_dma_1d_desc1, DESC_BYPASS_ON);   // for dram
+    dma_desc_set_bypassdst(_dma_1d_desc1, DESC_BYPASS_OFF);  // for vtcm
     dma_desc_set_length(_dma_1d_desc1, 0);
+
+    dma_desc_set_order(_dma_2d_desc0, DESC_ORDER_ORDER);
+    dma_desc_set_bypasssrc(_dma_2d_desc0, DESC_BYPASS_ON);   // for dram
+    dma_desc_set_bypassdst(_dma_2d_desc0, DESC_BYPASS_OFF);  // for vtcm
+    dma_desc_set_cachealloc(_dma_2d_desc0, 0);
+    dma_desc_set_roiwidth(_dma_2d_desc0, 0);
+    dma_desc_set_roiheight(_dma_2d_desc0, 0);
+    dma_desc_set_srcstride(_dma_2d_desc0, 0);
+    dma_desc_set_dststride(_dma_2d_desc0, 0);
+    dma_desc_set_srcwidthoffset(_dma_2d_desc0, 0);
+    dma_desc_set_dstwidthoffset(_dma_2d_desc0, 0);
 }
 
 dma_transfer::~dma_transfer() {
@@ -45,14 +56,11 @@ bool dma_transfer::submit1d(const uint8_t * src, uint8_t * dst, size_t size) {
     dma_desc_set_length(_dma_1d_desc0, size);
 
     void * buffs[] = { _dma_1d_desc0 };
-    _dma_desc_mutex.lock();
-    if (dma_desc_submit(buffs, std::size(buffs)) != DMA_SUCCESS) {
-        _dma_desc_mutex.unlock();
+    if (!submit_impl(buffs, std::size(buffs))) {
         DEVICE_LOG_ERROR("Failed to submit DMA descriptor\n");
         return false;
     }
 
-    _dma_desc_mutex.unlock();
     return true;
 }
 
@@ -75,14 +83,40 @@ bool dma_transfer::submit1d(const uint8_t * src0, uint8_t * dst0, const uint8_t 
     dma_desc_set_length(_dma_1d_desc1, size);
 
     void * buffs[] = { _dma_1d_desc0, _dma_1d_desc1 };
-    _dma_desc_mutex.lock();
-    if (dma_desc_submit(buffs, std::size(buffs)) != DMA_SUCCESS) {
-        _dma_desc_mutex.unlock();
+    if (!submit_impl(buffs, std::size(buffs))) {
         DEVICE_LOG_ERROR("Failed to submit DMA descriptor\n");
         return false;
     }
 
-    _dma_desc_mutex.unlock();
+    return true;
+}
+
+bool dma_transfer::submit2d(const uint8_t * src,
+                            uint8_t *       dst,
+                            size_t          width,
+                            size_t          height,
+                            size_t          src_stride,
+                            size_t          dst_stride) {
+    if (!dma_transfer::is_desc_done(_dma_2d_desc0)) {
+        DEVICE_LOG_ERROR("Failed to initiate DMA transfer for one or more descriptors\n");
+        return false;
+    }
+
+    dma_desc_set_next(_dma_2d_desc0, 0);
+    dma_desc_set_dstate(_dma_2d_desc0, DESC_DSTATE_INCOMPLETE);
+    dma_desc_set_src(_dma_2d_desc0, reinterpret_cast<uint32_t>(src));
+    dma_desc_set_dst(_dma_2d_desc0, reinterpret_cast<uint32_t>(dst));
+    dma_desc_set_roiwidth(_dma_2d_desc0, width);
+    dma_desc_set_roiheight(_dma_2d_desc0, height);
+    dma_desc_set_srcstride(_dma_2d_desc0, src_stride);
+    dma_desc_set_dststride(_dma_2d_desc0, dst_stride);
+
+    void * buffs[] = { _dma_1d_desc0 };
+    if (!submit_impl(buffs, std::size(buffs))) {
+        DEVICE_LOG_ERROR("Failed to submit DMA descriptor\n");
+        return false;
+    }
+
     return true;
 }
 
@@ -98,5 +132,12 @@ bool dma_transfer::is_desc_done(uint8_t * desc) {
 }
 
 qurt_mutex dma_transfer::_dma_desc_mutex = {};
+
+bool dma_transfer::submit_impl(void ** desc_batch, int batch_len) {
+    _dma_desc_mutex.lock();
+    const bool succ = dma_desc_submit(desc_batch, batch_len) == DMA_SUCCESS;
+    _dma_desc_mutex.unlock();
+    return succ;
+}
 
 }  // namespace hexagon::dma
--- ggml/src/ggml-qnn/npu/device/dma_transfer.hpp
+++ ggml/src/ggml-qnn/npu/device/dma_transfer.hpp
@@ -13,15 +13,23 @@ class dma_transfer {
 
     bool submit1d(const uint8_t * src, uint8_t * dst, size_t size);
     bool submit1d(const uint8_t * src0, uint8_t * dst0, const uint8_t * src1, uint8_t * dst1, size_t size);
+    bool submit2d(const uint8_t * src,
+                  uint8_t *       dst,
+                  size_t          width,
+                  size_t          height,
+                  size_t          src_stride,
+                  size_t          dst_stride);
     void wait();
 
   private:
-    static bool is_desc_done(uint8_t * desc);  // TODO: should we use void * here?
+    static bool       is_desc_done(uint8_t * desc);  // TODO: should we use void * here?
+    static qurt_mutex _dma_desc_mutex;
+
+    bool submit_impl(void ** desc_batch, int batch_len);
 
     alignas(DMA_DESC_SIZE_1D) uint8_t _dma_1d_desc0[DMA_DESC_SIZE_1D] = {};
     alignas(DMA_DESC_SIZE_1D) uint8_t _dma_1d_desc1[DMA_DESC_SIZE_1D] = {};
-
-    static qurt_mutex _dma_desc_mutex;
+    alignas(DMA_DESC_SIZE_2D) uint8_t _dma_2d_desc0[DMA_DESC_SIZE_2D] = {};
 
     DISABLE_COPY_AND_MOVE(dma_transfer);
 };
